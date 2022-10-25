d9c55d5e8101696df4e21c20ba78999e4dd5d3eb
bhashkar <bhashkar2600@gmail.com>
2022-10-25 12:23:06 +0530

    hello

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
- const hamburgar=document.getElementById("hamburgar");
- const x = document.getElementById("list");
- hamburgar.addEventListener("click",function(){
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-    
- })
+const hamburgar = document.getElementById("hamburgar");
+const x = document.getElementById("list");
+hamburgar.addEventListener("click", function () {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
 
